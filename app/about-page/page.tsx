8004fcc878137e0e52d5707982f2f53ae3a92fae
Vladimiro <vladimiro.cebotari@yacoobalabs.com>
2025-03-18 03:05:58 +0300

    chore: fixed one thing

--- app/about-page/page.tsx
+++ app/about-page/page.tsx
@@ -3,13 +3,9 @@
 import React from "react"
 import type { NextPage } from "next"
 import About from "@/libs/components/sections/index/About"
-import { Language } from "@/libs/components/theme/app/translations"
 
-interface HomeProps {
-  language: Language
-}
 
-const AboutPage : NextPage<HomeProps> = ({ language }) => {
+const AboutPage: NextPage = () => {
   return (
     <>
       <About />
